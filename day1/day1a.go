31478afe5de5341a50c94e16fc0634c8a60e7f29
Aaron <taykaiboon12@gmail.com>
2025-12-05 00:16:11 +0800

    day1

---
+++ day1/day1a.go
@@ -0,0 +1,48 @@
+package day1
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func Day1a() {
+	f, err := os.Open("./day1/day1.txt");
+	if err != nil {
+		fmt.Println(err);
+	}
+	reader := bufio.NewReader(f);
+	lock := 50;
+	pass := 0;
+	
+	for {
+		line, _, err := reader.ReadLine();
+
+		if err != nil {
+			break
+		}
+
+		dir := string(line[0]);
+		num, _ := strconv.Atoi(string(line[1:]));
+
+
+		if (dir == "L") {
+			lock -= num;
+		} else {
+			lock += num;
+		}
+
+		// if ans < 0, then add 100 to the ans
+		for lock < 0 {
+			lock += 100;
+		}
+
+		lock = lock % 100;
+
+		if lock == 0 {
+			pass += 1;
+		}
+	}	
+	fmt.Println(pass)
+}
